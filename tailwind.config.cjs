a480ca783be2cd9e3706cfafcecfecccc2783d9d
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-08-27 19:46:21 -0500

    feat: added files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      u8: ["U8-Bold", "sans-serif"],
+      general: ["General Sans", "sans-serif"],
+    },
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        // primary: "#e1bcff",
+        primary: "#cf95fe",
+        secondary: "#4400fe",
+        tertiary: "#1d123c",
+      },
+    },
+  },
+  plugins: [],
+};
